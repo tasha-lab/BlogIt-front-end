ea0130fe6e16fab3972cfccf8a29f9186ceb667f
Halima <natashawanjiku31@gmail.com>
2025-07-14 23:15:51 -0400

    add base url

--- src/Api/axios.ts
+++ src/Api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://blogit-db3x.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
